0a336f70578bc7c049a041c3d885fe343713fc0c
Atul-Gour <the.atulgour@gmail.com>
2026-03-05 10:00:00 +0530

    FloydWarshell implementaion done

---
+++ FloydWarshell.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class FloydWarshell {
+
+    static final int INF = (int)1e9;
+
+    public static void floydWarshall(int[][] graph, int V) {
+
+        int[][] dist = new int[V][V];
+
+        for (int i = 0; i < V; i++)
+            for (int j = 0; j < V; j++)
+                dist[i][j] = graph[i][j];
+
+        for (int k = 0; k < V; k++) {
+            for (int i = 0; i < V; i++) {
+                for (int j = 0; j < V; j++) {
+                    if (dist[i][k] + dist[k][j] < dist[i][j])
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][j] == INF)
+                    System.out.print("INF ");
+                else
+                    System.out.print(dist[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int V = 4;
+
+        int[][] graph = {
+            {0, 5, INF, 10},
+            {INF, 0, 3, INF},
+            {INF, INF, 0, 1},
+            {INF, INF, INF, 0}
+        };
+
+        floydWarshall(graph, V);
+    }
+}
